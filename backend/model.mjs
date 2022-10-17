8d7c2764470279dd786ad4e81e22b43f1f81f08c
Alexandra Rhodes <atperry30@gmail.com>
2022-10-17 11:22:41 -0700

    Added CRUD operations for Record and Resolution schemas

--- backend/model.mjs
+++ backend/model.mjs
@@ -14,6 +14,8 @@ db.once('open', () => {
     console.log('Successfully connected to MongoDB using Mongoose!');
 });
 
+/******* Record *******/
+
 // Define Record Schema
 const recordSchema = mongoose.Schema({
     recordID: {type: Number, required: true},
@@ -21,14 +23,49 @@ const recordSchema = mongoose.Schema({
     date: {type: String, required: true}
 });
 
-// Create model
+// Create Record Model
 const Record = mongoose.model("Record", recordSchema);
+
+// Define Create Record Function
+const createRecord = async (resolutionID, date) => {
+    const record = new Record({resolutionID:resolutionID, date: date});
+    return record.save();
+}
+
+// Define Delete Record Function
+const deleteRecord = async (recordID) => {
+    const result = await Record.deleteOne({recordID : recordID});
+    return result.deletedCount;
+}
+
+/******* Resolution *******/
 
 // Define Resolution Schema
 const resolutionSchema = mongoose.Schema({
     resolutionID: {type: Number, required: true},
-    goal: {type: Number, required: true}
+    name: {type: String, required: true},
+    goalCount: {type: Number, required: true}
 });
 
-// Create model
+// Create Resolution Model
 const Resolution = mongoose.model("Resolution", resolutionSchema);
+
+// Define Create Resolution Function
+const createResolution = async (resolutionID, name, goalCount) => {
+    const resolution = new Resolution({resolutionID: resolutionID, name: name, goalCount: goalCount});
+    return resolution.save();
+}
+
+// Define Update Resolution Function
+const updateResolution = async (resolutionID, name, goalCount) => {
+    const result = await Resolution.replaceOne(
+        {resolutionID: resolutionID},
+        {name: name, goalCount: goalCount});
+    return result.modifiedCount;
+}
+
+// Define Delete Resolution Function
+const deleteResolution = async (resolutionID) => {
+    const result = await Resolution.deleteOne({resolutionID: resolutionID});
+    return result.deletedCount;
+}

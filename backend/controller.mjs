772eb9c1f7a5367647d9cf60fe05978abf69f4a2
Alexandra Rhodes <atperry30@gmail.com>
2022-10-17 11:59:22 -0700

    Added create record logic to controller

--- backend/controller.mjs
+++ backend/controller.mjs
@@ -1,9 +1,29 @@
+// Import required packages
 import express from 'express';
+
+// Import models
+import * as record from 'models/record_model.mjs';
+import * as resolution from 'models/resolution_model.mjs';
+
+// Establish variables
 const app = express();
 const PORT = 3000;
 
 app.use(express.json())
 
+// Confirm sever is running by printing to terminal
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
+});
+
+// Create a new record
+app.post('/records', (req, res) => {
+    record.createRecord(req.body.recordID, req.body.date)
+    .then(record => {
+        res.status.json(record);
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).json({Error: 'Request Failed'})
+    })
 });

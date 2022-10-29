d08e24dfe715e6867e40dc49d83e1d4740205606
Alexandra Rhodes <atperry30@gmail.com>
2022-10-29 13:56:55 -0700

    Changed Resolution/Record model from reference to embedded

--- backend/models/resolution_model.mjs
+++ backend/models/resolution_model.mjs
@@ -3,27 +3,45 @@ import mongoose from 'mongoose';
 
 // Define resolution schema
 const resolutionSchema = mongoose.Schema({
-    resolutionID: {type: Number, required: true},
+    //resolutionID: {type: Number, required: true},
     name: {type: String, required: true},
-    goalCount: {type: Number, required: true}
+    goalCount: {type: Number, required: true},
+    color: {type: String, required: true},
+    records: {type: Array, required: true}
 });
 
 // Create resolution model
 const Resolution = mongoose.model("Resolution", resolutionSchema);
 
+
 // Define Create Resolution Function
-const createResolution = async (resolutionID, name, goalCount) => {
-    const resolution = new Resolution({resolutionID: resolutionID, name: name, goalCount: goalCount});
+const createResolution = async (name, goalCount, color) => {
+    const resolution = new Resolution({name: name, goalCount: goalCount, color: color, records: []});
     return resolution.save();
 };
 
 // Define update resolution function
-const updateResolution = async (resolutionID, name, goalCount) => {
-    const result = await Resolution.replaceOne(
+const updateResolution = async (resolutionID, name, goalCount, color) => {
+    const result = await Resolution.updateOne(
         {resolutionID: resolutionID},
-        {name: name, goalCount: goalCount});
+        {name: name, goalCount: goalCount, color: color});
     return result.modifiedCount;
 };
+
+const addResolutionRecord = async (resolutionID, record) => {
+    const result = await Resolution.updateOne(
+        {resolutionID: resolutionID},
+        {$push: {record: record}});
+    return result.modifiedCount;
+};
+
+const deleteResolutionRecord = async (resolutionID, record) => {
+    const result = await Resolution.updateOne(
+        {resolutionID: resolutionID},
+        {$pull: {record: record}});
+    return result.modifiedCount;
+};
+
 
 // Define delete resolution function
 const deleteResolution = async (resolutionID) => {
